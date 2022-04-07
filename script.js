00ac671a9b07e0d4726812d50393f0e1b358a8c5
nexus122 <juan-pablo122@hotmail.es>
2022-04-07 23:31:19 +0200

    Cambios en los filtros

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 Vue.createApp({
     data() {
         return {
-            userName: 'nexus122', // Cambiame y la pagina cambiara entera
+            // Config
+            userName: 'nexus122', // Cambiame y la pagina cambiara entera            
+            homepageProjects: false,
+            btnText: 'Solo proyectos con demo',
+            // Data
             githubRepos: [],
             userInfo: []
         }
     },
     methods: {
         async fetchGithubRepos() {
-            let result = await fetch(`https://api.github.com/users/${this.userName}/repos`);
+            let result = await fetch(`https://api.github.com/users/${this.userName}/repos?type=all&per_page=1000`);
             this.githubRepos = await result.json();
         },
         async fetchUserInfo() {
             let result = await fetch(`https://api.github.com/users/${this.userName}`);
-            this.userInfo = await result.json();
-        }        
+            this.userInfo = await result.json();            
+        }
     },
     mounted() {
         this.fetchGithubRepos();
